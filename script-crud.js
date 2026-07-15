25f78be9c56f49ef743f98135d3f525b76f25ce0
Murilo Vinicius <murilovinicius.pro@gmail.com>
2026-07-14 23:02:00 -0300

    usa o localstorage para armazenar uma tarefa adicionada ao array de tarefas

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
 const btnAdcTask = document.querySelector(".app__button--add-task");
 const formVisible = document.querySelector(".app__form-add-task");
+const textarea = document.querySelector(".app__form-textarea");
+
+const tarefas = [];
 
 btnAdcTask.addEventListener("click", () => {
   formVisible.classList.toggle("hidden");
+});
+
+formVisible.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const tarefa = {
+    descricao: textarea.value,
+  };
+  tarefas.push(tarefa);
+  localStorage.setItem("tarefas", tarefas);
 });
