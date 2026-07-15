c07d757ddbe7844ca5e893967dad4597492ee840
Murilo Vinicius <murilovinicius.pro@gmail.com>
2026-07-15 00:47:39 -0300

    atualiza descricao da tarefa

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,10 @@ const ulTarefas = document.querySelector(".app__section-task-list");
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
+const atualizarTarefas = () => {
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+};
+
 const criarElementoTarefe = (tarefa) => {
   const li = document.createElement("li");
   li.classList.add("app__section-task-list-item");
@@ -22,6 +26,13 @@ const criarElementoTarefe = (tarefa) => {
 
   const botao = document.createElement("button");
   botao.classList.add("app_button-edit");
+
+  botao.onclick = () => {
+    const novaDescricao = prompt("Qual é o novo nome da tarefa?");
+    paragrafo.textContent = novaDescricao;
+    tarefa.descricao = novaDescricao;
+    atualizarTarefas();
+  };
 
   const imagemBotao = document.createElement("img");
   imagemBotao.setAttribute("src", "/imagens/edit.png");
@@ -47,7 +58,7 @@ formVisible.addEventListener("submit", (evento) => {
   tarefas.push(tarefa);
   const elementoTarefa = criarElementoTarefe(tarefa);
   ulTarefas.append(elementoTarefa);
-  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  atualizarTarefas();
   textarea.value = "";
   formVisible.classList.add("hidden");
 });
